914fe836fc0ecc7e968588076aac98d11f3991c9
Vincent Klimjack <vklimjack@gmail.com>
2024-09-01 13:50:38 +0100

    seed library on page load

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
+const myLibrary = [];
+
 function Book(title, author, pageCount, read) {
   this.title = title;
   this.author = author;
   this.pageCount = pageCount;
   this.read = read;
   this.info = function () {
-    return `${this.title} by ${this.author}, ${pageCount} pages, ${this.read === true ? "read it" : "not read yet"}.`
+    return `${this.title} by ${this.author}, ${pageCount} pages, ${this.read === true ? "read it" : "not read yet"}.`;
   };
-};
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function seedLibrary() {
+  for (let index = 0; index < 10; index++) {
+    let book = new Book(`Test Book ${index + 1}`, "Test Author", 100, index % 2 === 0);
+
+    addBookToLibrary(book);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  seedLibrary();
+});
